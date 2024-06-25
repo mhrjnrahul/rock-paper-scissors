afea9ad5376c1a7c7196e23b83a0d56ec1fed558
mhrjnrahul <maharjanrahul58@gmail.com>
2024-06-25 19:18:38 +0545

    added functions for the game

---
+++ script.js
@@ -0,0 +1,60 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice(){
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random()*choices.length);
+    return choices[randomIndex];
+}
+
+console.log(getComputerChoice());
+
+function getHumanChoice(){
+    const choices = ["rock", "paper", "scissors"];
+    let userChoice = null;
+
+    while(!userChoice){
+        const input = prompt("Enter your choice (rock, paper, scisors)");
+
+        //if user cancels the prompt
+        if(input == null){
+            alert("user cancelled the prompt");
+            return null;
+        }
+
+        const normalizedInput = input.toLowerCase().trim();
+
+        if(choices.includes(normalizedInput)){
+            userChoice = normalizedInput;
+        } else{
+            alert("Input valid choice");
+        }
+    }
+
+    return userChoice;
+}
+
+function playRound(computerChoice, humanChoice){
+    if(computerChoice == humanChoice){
+        console.log("Same choices, next round");
+    }
+
+    if(computerChoice == "rock" && humanChoice == "scissors"){
+        console.log("rock beats scissors, computer wins");
+    } else if(computerChoice == "paper" && humanChoice == "rock"){
+        console.log("paper beats rock, computer wins");
+    } else if(computerChoice == "scissors" && humanChoice == "paper"){
+        console.log("scissors beats paper, computer wins");
+    } else if(computerChoice == "scissors" && humanChoice == "rock"){
+        console.log("rock beats scissors, human wins");
+    } else if(computerChoice == "paper" && humanChoice == "scissors"){
+        console.log("scissors beats paper, human wins");
+    } else{
+        console.log("paper beats rock, human wins");   
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
